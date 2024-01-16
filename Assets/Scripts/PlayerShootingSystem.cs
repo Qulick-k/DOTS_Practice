1a19a2702caf8d78778e5951a0800dee9a5b98b7
qulick <58820014+Qulick-k@users.noreply.github.com>
2024-01-17 00:33:26 +0800

    Spawing Entities with Entity Command Buffer (Structural Changes)
    [Add] 新增PlayerShootingSystem腳本，模擬玩家射出子彈，內有註解如何使用Entity Command Buffer來生成Entities

---
+++ Assets/Scripts/PlayerShootingSystem.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+//此腳本模擬玩家射出子彈的情況，因此新增PlayerShootingSystem腳本，使用partial class，並且繼承SystemBase
+public partial class PlayerShootingSystem : SystemBase
+{
+    //確保只跑在Player tag component上
+    protected override void OnCreate()
+    {        
+        RequireForUpdate<Player>();
+    }
+
+    
+    //假設讓Cube當作子彈，然後用簡單的按鍵檢測玩家有沒有按空白鍵，如果玩家沒有按空白鍵就return
+    protected override void OnUpdate()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space))
+        {
+            return;
+        }
+
+        SpawnCubesConfig spawnCubesConfig = SystemAPI.GetSingleton<SpawnCubesConfig>();
+
+
+        //新增一個new EntityCommandBuffer();，EntityCommandBuffer需要一個特定的記憶體分配器，有許多選項能選擇，端看EntityCommandBuffer要留存多久
+        //EntityCommandBuffer在這個腳本只用來生成entity，然後就把EntityCommandBuffer丟棄，所以使用temp，temp只會讓EntityCommandBuffer留存1 frame或是 a job，之後就直接丟棄
+        //如果要榨取更細微的效能的話，可以使用new EntityCommandBuffer(WorldUpdateAllocator);
+        EntityCommandBuffer entityCommandBuffer = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
+
+        #region 註解 
+        //尋找玩家的位置，讓子彈知道該在哪裡生成。採用唯讀RefRO即可，和使用WithAll<Player>()指定Player的tag component
+        //如果要生成新的Entity的話，就要調整下面的Entity spawnedEntity的程式碼了
+        //但如果不想要調整的話，為了在Job或是foreacj生成或是摧毀，可以使用EntityCommandBuffer
+        //如果new prefab沒有match到 SystemAPI.Query<RefRO<LocalTransform>>().WithAll<Player>()的話，就能正常運行下面的程式碼。
+        //像是這腳本要spawn一個cube entity，那就會沒有player component。而如果已經被生成的entity的type符合 SystemAPI.Query<RefRO<LocalTransform>>().WithAll<Player>()的話，就無法運作
+        //所以應該用EntityCommandBuffer來生成Entity。        
+        #endregion
+        foreach (RefRO<LocalTransform> localTramsform in SystemAPI.Query<RefRO<LocalTransform>>().WithAll<Player>())
+        {
+            //entityCommandBuffer並沒有真的生成entity，而是新增一個command到command list表裡面，之後才會執行command list
+            //呼叫entityCommandBuffer.SetComponent()，然後把spawnedEntity和LocalTransform當作輸入參數放進去            
+            Entity spawnedEntity = entityCommandBuffer.Instantiate(spawnCubesConfig.cubePrefabEntity); 
+            entityCommandBuffer.SetComponent(spawnedEntity, new LocalTransform  //這只是登錄一個command，不會真的設立任何的component data
+            {
+                Position = localTramsform.ValueRO.Position,
+                Rotation = quaternion.identity,
+                Scale = 1F
+            });
+        }
+
+        //當佇列都設定好了，就可以執行command list。
+        //呼叫Playback()，就能拿EntityManager當參數，讓EntityManager生成entity了
+        entityCommandBuffer.Playback(EntityManager);
+    }
+}
